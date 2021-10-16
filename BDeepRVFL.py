c0560e8ce067bd04382c16186b3a446ac241d92f
Yaoc0009 <yeo2octave27@gmail.com>
2021-10-16 18:39:10 +0800

    BDeepRVFL.py

--- BDeepRVFL.py
+++ BDeepRVFL.py
@@ -4,26 +4,28 @@ from scipy.io import loadmat
 import pymc3 as pm
 
 n_node = 10 # num of nodes in hidden layer
+n_layer = 5 # num of hidden layers
 w_range = [-1, 1] # range of random weights
 b_range = [0, 1] # range of random biases
 
-class BRVFL:
-    """ BRVFL Classifier """
+class BayesianDeepRVFL:
+    """ Bayesian Deep RVFL Classifier """
 
-    def __init__(self, n_node, w_range, b_range, alpha_1=10**(-5), alpha_2=10**(-5), alpha_3=10**(-5), alpha_4=10**(-5), n_iter=1000, tol=1.0e-3, activation='relu'):
+    def __init__(self, n_node, w_range, b_range, n_layer, alpha_1=10**(-5), alpha_2=10**(-5), alpha_3=10**(-5), alpha_4=10**(-5), n_iter=1000, tol=1.0e-3, activation='relu', same_feature=False):
         self.n_node = n_node
         self.w_range = w_range
         self.b_range = b_range
+        self.n_layer = n_layer
         
-        self.alpha_1 = alpha_1 # Gamma distribution parameter
+        self.alpha_1 = alpha_1 # Gamma distribution parameters
         self.alpha_2 = alpha_2
         self.alpha_3 = alpha_3
         self.alpha_4 = alpha_4
         self.n_iter = n_iter
         self.tol = tol
 
-        self.weight = None
-        self.bias = None
+        self.weight = []
+        self.bias = []
         self.beta = None
         self.prec = None
         self.var = None
@@ -31,26 +33,27 @@ class BRVFL:
         self.activation_function = getattr(a, activation)
         self.covar = None
         self.mean = None
+        self.data_std = [None] * self.n_layer
+        self.data_mean = [None] * self.n_layer
+        self.same_feature = same_feature
 
     def train(self, data, label, n_class):
         assert len(data.shape) > 1
         assert len(data) == len(label)
         assert len(label.shape) == 1
 
-        data = self.standardize(data)
-        # print('Shape of data:', np.shape(data))
         n_sample, n_feature = np.shape(data)
-        self.weight = (self.w_range[1] - self.w_range[0]) * np.random.random([n_feature, self.n_node]) + self.w_range[0]
-        self.bias = (self.b_range[1] - self.b_range[0]) * np.random.random([1, self.n_node]) + self.b_range[0]
-        # print('Shape of weights:', np.shape(self.weight))
-        # print('Shape of bias:', np.shape(self.bias))
-        
-        h = self.activation_function(np.dot(data, self.weight) + np.dot(np.ones([n_sample, 1]), self.bias))
-        d = np.concatenate([h, data], axis=1)
-        # d = np.concatenate([d, np.ones_like(d[:, 0:1])], axis=1)
-        # print('Shape of D:', np.shape(d))
+        d = self.standardize(data, 0) # Normalize
+        h = data.copy()
+        for i in range(self.n_layer):
+            h = self.standardize(h, i)
+            self.weight.append((self.w_range[1] - self.w_range[0]) * np.random.random([len(h[0]), self.n_node]) + self.w_range[0])
+            self.bias.append((self.b_range[1] - self.b_range[0]) * np.random.random([1, self.n_node]) + self.b_range[0])
+            h = self.activation_function(np.dot(h, self.weight[i]) + np.dot(np.ones([n_sample, 1]), self.bias[i]))
+            d = np.concatenate([h, d], axis=1)
+
+        # d = np.concatenate([d, np.ones_like(d[:, 0:1])], axis=1) # concat column of 1s
         y = self.one_hot_encoding(label, n_class)
-        # print('Shape of y:', np.shape(y))
         dT_y = np.dot(d.T, y)
         dT_d = np.dot(d.T, d)
         eigen_val = np.linalg.eigvalsh(dT_d)
@@ -60,7 +63,7 @@ class BRVFL:
         with model:
             p = pm.Gamma('p', alpha=self.alpha_1, beta=self.alpha_2)
             v = pm.Gamma('v', alpha=self.alpha_3, beta=self.alpha_4)
-            b = pm.Normal('b', mu=0, tau=p, shape=(n_feature + self.n_node + 1, n_class))
+            b = pm.Normal('b', mu=0, tau=p, shape=(len(d[0]), n_class))
             y_obs = pm.Normal('y_obs', mu=pm.math.dot(d, b), tau=v, observed=y)
         
         map_estimate =  pm.find_MAP(model=model)
@@ -105,9 +108,14 @@ class BRVFL:
         # print('Variance: ', self.var)
 
     def predict(self, data, raw_output=False):
-        data = self.standardize(data) # Normalize
-        h = self.activation_function(np.dot(data, self.weight) + self.bias)
-        d = np.concatenate([h, data], axis=1)
+        n_sample = len(data)
+        d = self.standardize(data, 0) # Normalize
+        h = data.copy()
+        for i in range(self.n_layer):
+            h = self.standardize(h, i)
+            h = self.activation_function(np.dot(h, self.weight[i]) + np.dot(np.ones([n_sample, 1]), self.bias[i]))
+            d = np.concatenate([h, d], axis=1)
+
         # d = np.concatenate([d, np.ones_like(d[:, 0:1])], axis=1)
         result = self.softmax(np.dot(d, self.beta))
         if not raw_output:
@@ -119,12 +127,7 @@ class BRVFL:
         assert len(data) == len(label)
         assert len(label.shape) == 1
         
-        data = self.standardize(data)  # Normalize
-        h = self.activation_function(np.dot(data, self.weight) + self.bias)
-        d = np.concatenate([h, data], axis=1)
-        # d = np.concatenate([d, np.ones_like(d[:, 0:1])], axis=1)
-        result = np.dot(d, self.beta)
-        result = np.argmax(result, axis=1)
+        result = self.predict(data, False)
         acc = np.sum(np.equal(result, label))/len(label)
         return acc
 
@@ -134,10 +137,19 @@ class BRVFL:
             y[i, label[i]] = 1
         return y
 
-    def standardize(self, x):
-        std = np.maximum(np.std(x, axis=0), 1/np.sqrt(len(x)))
-        mean = np.mean(x, axis=0)
-        return (x - mean) / std
+    def standardize(self, x, index):
+        if self.same_feature is True:
+            if self.data_std[index] is None:
+                self.data_std[index] = np.maximum(np.std(x), 1/np.sqrt(len(x)))
+            if self.data_mean[index] is None:
+                self.data_mean[index] = np.mean(x)
+            return (x - self.data_mean[index]) / self.data_std[index]
+        else:
+            if self.data_std[index] is None:
+                self.data_std[index] = np.maximum(np.std(x, axis=0), 1/np.sqrt(len(x)))
+            if self.data_mean[index] is None:
+                self.data_mean[index] = np.mean(x, axis=0)
+            return (x - self.data_mean[index]) / self.data_std[index]
 
     def softmax(self, x):
         return np.exp(x) / np.repeat((np.sum(np.exp(x), axis=1))[:, np.newaxis], len(x[0]), axis=1)
@@ -173,18 +185,19 @@ if __name__=="__main__":
         train_index, val_index = kf_values
         X_val_train, X_val_test = X_train[train_index], X_train[val_index]
         y_val_train, y_val_test = y_train[train_index], y_train[val_index]
-        brvfl = BRVFL(n_node, w_range, b_range)
-        brvfl.train(X_val_train, y_val_train, n_class)
-        prediction = brvfl.predict(X_val_test, True)
-        acc = brvfl.eval(X_val_test, y_val_test)
+        bdrvfl = BayesianDeepRVFL(n_node, w_range, b_range, n_layer)
+        bdrvfl.train(X_val_train, y_val_train, n_class)
+        prediction = bdrvfl.predict(X_val_test, True)
+        acc = bdrvfl.eval(X_val_test, y_val_test)
         print(f'Validation accuracy: {acc}')
         val_acc.append(acc)
         if acc >= max(val_acc):
             max_index = train_index
 
     X_train, y_train = X_train[max_index], y_train[max_index]
-    brvfl = BRVFL(n_node, w_range, b_range)
-    brvfl.train(X_train, y_train, n_class)
-    prediction = brvfl.predict(X_test, True)
-    acc = brvfl.eval(X_test, y_test)
+    print(np.shape(X_train))
+    bdrvfl = BayesianDeepRVFL(n_node, w_range, b_range, n_layer)
+    bdrvfl.train(X_train, y_train, n_class)
+    prediction = bdrvfl.predict(X_test, True)
+    acc = bdrvfl.eval(X_test, y_test)
     print(f'\nTest accuracy: {acc}')
